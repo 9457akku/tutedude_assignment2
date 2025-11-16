c7dba4497a22c0e1a4408f452f6b84cbc937e0ec
pythontechidea <pythontechidea@gmail.com>
2025-11-17 01:05:44 +0530

    initial commit

---
+++ Task1.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter Second number: "))
+
+sum = num1+num2
+mul= num1*num2
+div= num1/num2
+sub= num1-num2
+
+print(" Addition : ", sum)
+print(" Subtraction : ", sub)
+print(" Multiplication : ", mul)
+print(" Division : ", div)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+fname = input("Enter your First Name: ")
+lname = input("Enter your Last Name: ")
+full_name = fname+" "+lname
+print("Hello, ",full_name,"! Welcome to the Python program.")
