70234cd8262b111ec2b57494fdc8e69e3d04119a
pythontechidea <pythontechidea@gmail.com>
2025-11-18 23:10:36 +0530

    initial comit for odd even program

--- Task1.py
+++ Task1.py
@@ -1,12 +1,6 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter Second number: "))
+num = int(input(f"Enter a number: "))
 
-sum = num1+num2
-mul= num1*num2
-div= num1/num2
-sub= num1-num2
-
-print(" Addition : ", sum)
-print(" Subtraction : ", sub)
-print(" Multiplication : ", mul)
-print(" Division : ", div)
+if num%2==0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
